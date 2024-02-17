3dc958af623331474f82c5563c5580c97191bc9e
renlua <141826500+renlua@users.noreply.github.com>
2024-02-17 17:40:32 +0800

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,1 +1,12 @@
+if R == "忍脚本交流群:139341298" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/lua/main/renscript.lua"))();
+elseif name == "SyndromeXHUB" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SyndromeXph/Hub/main/Hubkey"))()
+else
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "脚本",
+    Text = "错误链接",
+    Duration = 5,
+})
+end
