b44b2d0394178a51a627b4039c4269140f8de6a5
renlua <141826500+renlua@users.noreply.github.com>
2024-04-04 23:51:17 +0800

    Update renscript.lua

--- renscript.lua
+++ renscript.lua
@@ -7483,8 +7483,8 @@ Tab:Button("星月交辉 V3[破解]", function()
     end)
     
     Tab:Button("道庭DT[破解]", function()
-    game.Players.LocalPlayer.Name = "4v5n7n"
-    game.Players.LocalPlayer.Character.Name = "4v5n7n"
+    game.Players.LocalPlayer.Name = "123fa98"
+    game.Players.LocalPlayer.Character.Name = "123fa98"
     getgenv().DT = "道庭DT脚本群811468839"
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\52\118\53\110\55\110\47\108\108\108\47\109\97\105\110\47\49\51\56\50\50\56\56\57\46\108\117\97\34\41\41\40\41\9\10")("道庭DT V2.0")
     end)
