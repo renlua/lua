37beb5269c6398ed328d8427d8ea6510b42bc570
renlua <141826500+renlua@users.noreply.github.com>
2024-04-05 16:48:47 +0800

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,12 +1,1 @@
-if R == "忍脚本交流群:139341298" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/lua/main/renscript.lua"))();
-elseif R == "忍脚本【免费版】" then
-loadstring(game:HttpGet("https://shz.al/wQcr"))();
-else
-local CoreGui = game:GetService("StarterGui")
-CoreGui:SetCore("SendNotification", {
-    Title = "脚本",
-    Text = "错误链接",
-    Duration = 5,
-})
-end
+
