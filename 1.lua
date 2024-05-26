cb115f3b2f1030e6a524abf095a66b3c3c492ffc
renlua <141826500+renlua@users.noreply.github.com>
2024-05-26 09:46:30 +0800

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,17 +1,12 @@
 local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/lua/main/renscript.lua"))();
 
-local api = game:HttpGet("https://users.roblox.com/v1/users/"..game.Players.LocalPlayer.UserId)
-local REN_name = string.match(api,'name":"(.-)",')
-local REN_displayName = string.match(api,'displayName":"(.-)"')
-
 if game:GetService("CoreGui").BubbleChat["BubbleChat_"..game.Players.LocalPlayer.UserId].padding.Name == "padding" then
-    if game.Players.LocalPlayer.Name == game.Players.LocalPlayer.Character.Name and game.Players.LocalPlayer.DisplayName == REN_displayName and game.Players.LocalPlayer.Name == REN_name and game.Players.LocalPlayer.Character.Name == REN_name and game.Players.LocalPlayer.CharacterAppearanceId == game.Players.LocalPlayer.UserId then
+    if game.Players.LocalPlayer.Name == game.Players.LocalPlayer.Character.Name and game.Players.LocalPlayer.CharacterAppearanceId == game.Players.LocalPlayer.UserId then
         if whitelist[game.Players.LocalPlayer.Name] then
             setclipboard("破解成功")
         end
     end
 end
-
 
 
 
