1d0305420acb4d0e3074c9f51ae940ea96034fa8
renlua <141826500+renlua@users.noreply.github.com>
2024-07-06 22:38:02 +0800

    Update 白名单.lua

--- 白名单.lua
+++ 白名单.lua
@@ -2,7 +2,7 @@ return {
   ["123fa98"] = "7c10ebe5-57d9-416f-8eea-2788b13f7527",
   ["xszxwhbdd"] = "147fbe68-4e12-4403-8998-a1ff68cba12c",
 
-  ["名字"] = "ID",
+  ["Roblox_OldSix666"] = "d7cbef33-7a5b-4331-b3f0-a38b6db75cae",
   ["名字"] = "ID",
   ["名字"] = "ID",
   ["名字"] = "ID",
